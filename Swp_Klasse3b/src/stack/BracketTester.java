7b5f7a845851730f245a7e961e9edbe9cef96941
juliapetz <juliapetz@juliapetz-VirtualBox>
2024-04-23 09:20:22 +0200

    stack exercises solution

---
+++ Swp_Klasse3b/src/stack/BracketTester.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.LinkedList;
+
+public class BracketTester {
+	
+	public static boolean checkBrackets(String input) {
+		LinkedList<Character> stack = new LinkedList<>();
+		for(char c : input.toCharArray()) {
+			if(c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			} else if(c == ')' || c == '}' || c == ']') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char lastOpen = stack.peek();
+				if((c == ')' && lastOpen != '(') ||
+					(c == ']' && lastOpen != '[') ||
+					(c == '}' && lastOpen != '{')) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
